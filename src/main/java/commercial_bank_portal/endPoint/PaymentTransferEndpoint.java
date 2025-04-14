33bb11d8761d8e816621afb6184d86930108160a
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-04-14 13:11:50 +0530

    changes

--- src/main/java/commercial_bank_portal/endPoint/PaymentTransferEndpoint.java
+++ src/main/java/commercial_bank_portal/endPoint/PaymentTransferEndpoint.java
@@ -1,6 +1,6 @@
 package commercial_bank_portal.endPoint;
 
-import commercial_bank_portal.generated.jaxb.com.commercialbankportal.banktransfer.BankRequest;
+import commercial_bank_portal.generatedSource.DocumentRequest;
 import commercial_bank_portal.service.PaymentTransferService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -15,8 +15,8 @@ public class PaymentTransferEndpoint {
 
     private final PaymentTransferService paymentTransferService;
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "BankRequest")
-    public void handleTransfer(@RequestPayload BankRequest request) throws Exception {
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "DocumentRequest")
+    public void handleTransfer(@RequestPayload DocumentRequest request) throws Exception {
         System.out.println("gfdgfdgfd");
     }
 }
